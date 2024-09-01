d1c8adae65d03cce7da77ece70a96ffc1437df01
Carlos Cardoso <carloseliezerdev@gmail.com>
2024-09-01 10:53:08 -0300

    new component footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 export default function Home(){
-    return (<Header></Header>)
+    return (
+        <main>
+            <Header></Header>
+            <Footer></Footer>
+        </main>
+)
 }
