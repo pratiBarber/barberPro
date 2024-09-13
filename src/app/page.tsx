69827303d9a36bbb04fc8559de38864f458146d4
Carlos Cardoso <carloseliezerdev@gmail.com>
2024-09-13 15:23:46 -0300

    new page 1/2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Homee from "./home/Home"
 
 
 export default function Home(){
     return (
         <main>
             <Header></Header>
+            <Homee></Homee>
             <Footer></Footer>
         </main>
 )
