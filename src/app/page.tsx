cc6cb9a2661b63310b6f6438605187c6e8c07a96
ryoumaster <pferreiraduraes@gmail.com>
2024-11-05 18:11:16 -0300

    Seleção de barbeiros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Header from './components/Header'
 import Home from './home/Home'
 import Footer from './components/Footer'
-import Feedback from './review/page'
+import Select from './select/Select'
 
 export default function App(){
     return (
         <main>
-            <Header></Header>
-            <Home></Home>
-            <Footer></Footer>
+            <Select></Select>
         </main>
 )
 }
